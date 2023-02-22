0d8565e16ee553f6dd218f05654a2dbb8a09f8af
niugy233 <2783439652@QQ.com>
2023-02-22 20:15:23 +0800

    数的三次方根

---
+++ AcWing/Recursion/Dichotomy/790.cpp
@@ -0,0 +1,28 @@
+/* 数的三次方根 */
+#include <bits/stdc++.h>
+using namespace std;
+
+double target;
+double cube(double num){
+    return (num * num * num);
+}
+double search(){
+    double l = -10000,r = target;
+    double mid = l + (r - l) / 2.0;
+    while(r-l >= 1e-7){ //由于本题是取得小数点后六位当r-l的精度小于小数点后八位时结束循环
+        if(target <= cube(mid)){
+            // 这里的边界问题怎么解决
+            r = mid;
+        }
+        else{
+            l = mid;
+        }
+        mid = l + (r - l) / 2.0;
+    }
+    return l;
+}
+
+int main(){
+    cin >> target;
+    cout << fixed << setprecision(6) << search();
+}

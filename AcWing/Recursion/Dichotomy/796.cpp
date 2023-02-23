7d5f240312d40a25f5aa7e3a600d07bbf82638f1
niugy233 <2783439652@QQ.com>
2023-02-23 14:00:25 +0800

    浅醉和

---
+++ AcWing/Recursion/Dichotomy/796.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 矩阵n*m
+int n,m,q;
+int a,b,c,d;
+
+int matrix[1005][1005];
+// 储存从1，1到i，j的矩阵和
+int summatrix[1005][1005];
+
+void CalcSumMatrix(){
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=m;j++){
+            summatrix[i][j] = summatrix[i][j - 1] + matrix[i][j] + summatrix[i-1][j] - summatrix[i-1][j-1];
+        }
+    }
+}
+int calcres(){
+    cout << c << d << ' ' << c-a - 1<< d  << ' ' << c << d-b - 1 << ' ' << a-1 << b- 1<< endl;
+    return (summatrix[c][d] - (summatrix[a - 1][d] + summatrix[c][b - 1]) + summatrix[a-1][b-1]); 
+}
+
+int main(){
+    cin >> n >> m >>  q;
+    memset(summatrix,0,sizeof(summatrix));
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j <= m;j++){
+            cin >> matrix[i][j];
+        }
+    }
+    CalcSumMatrix();
+    while(q--){
+        cin >> a >> b >> c >> d;    
+        cout << calcres() << endl; 
+    }
+}
